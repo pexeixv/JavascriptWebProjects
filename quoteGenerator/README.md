401dfe074da79c2d88bd32eeceb46c3d3dc6d379
Gavin Pereira <gavineditz@gmail.com>
2021-03-18 20:06:52 +0530

    Quotes Generator added

---
+++ quoteGenerator/README.md
@@ -0,0 +1,16 @@
+# Quote Generator
+
+## Overview
+* A simple web application that generates and displays random quotes.
+
+## Features
+* Mobile Responsive.
+* 'New Quote' generator button.
+* 'Tweet' button to directly tweet quotes.
+
+## Resources used
+* Quotes pulled from [Forismatic API](https://forismatic.com/en/api/).
+* Icons from [Font Awesome](https://fontawesome.com/).
+* ['Poppins'](https://fonts.google.com/specimen/Poppins) typeface from [Google Fonts](https://fonts.google.com/).
+* Background pattern from [Hero Patterns](https://www.heropatterns.com/).
+* Direct tweet button added using Twitter's [Web Intent URL](https://developer.twitter.com/en/docs/twitter-for-websites/tweet-button/guides/web-intent).
---
+++ quoteGenerator/js/script.js
@@ -0,0 +1,57 @@
+// Initialise variables
+var quoteContainer = document.querySelector('.quote_container');
+var quote = document.querySelector('#quote');
+var author = document.querySelector('#author');
+var twitter = document.querySelector('.twitter');
+var newQuote = document.querySelector('#quote_new');
+var loader = document.querySelector('.loader');
+
+var showLoadingAnimation = flag => {
+    if (flag) {
+        loader.style.display = "flex";
+        quoteContainer.style.display = "none";
+    }
+    else {
+        loader.style.display = "none";
+        quoteContainer.style.display = "flex";
+    }
+}
+
+
+// Get quote from API
+var getQuoteFromAPI = async () => {
+    var URL = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try {
+        showLoadingAnimation(true);
+        var res = await fetch(URL);
+        var data = await res.json();
+        quote.innerText = data.quoteText;
+        if (data.quoteText.length > 100)
+            quote.classList.add('quote_text-long');
+        else
+            quote.classList.remove('quote_text-long');
+        if (data.quoteAuthor)
+            author.innerText = data.quoteAuthor;
+        else
+            author.innerText = 'Anonymous';
+        showLoadingAnimation(false);
+
+    } catch (error) {
+        getQuoteFromAPI();
+        console.error(`We're sorry, quote not found - ${error}`);
+    }
+}
+getQuoteFromAPI();
+
+var tweetQuoteInNewTab = () => {
+    var quoteText = quote.innerText;
+    var authorText = author.innerText;
+    var twitterBaseUrl = `https://twitter.com/intent/tweet?text=`;
+    var twitterUrl = `${twitterBaseUrl}"${quoteText}" - ${authorText}`;
+    console.log(twitterUrl);
+    window.open(twitterUrl, '_blank');
+}
+
+twitter.addEventListener('click', tweetQuoteInNewTab);
+newQuote.addEventListener('click', getQuoteFromAPI);
+
